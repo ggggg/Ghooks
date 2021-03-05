794735fd31918a72f9d2aa399d92ba067483fedb
Ido <benhaim.ido@gmail.com>
2021-03-05 17:32:44 -0500

    Trying out new config options

--- src/Webhooks/Events/OnCrime.cs
+++ src/Webhooks/Events/OnCrime.cs
@@ -10,9 +10,8 @@ namespace Webhooks.Events
         public void OnEvent(ShPlayer player, byte crimeIndex, ShPlayer victim)
         {
             if (!player.isHuman || player.svPlayer.godMode || player.svPlayer.InvalidCrime(crimeIndex)) return;
-            Crime crime = player.manager.GetCrime(crimeIndex);
-            Core.Instance.CrimeWebhook.Send(string.Format(Core.Instance.Settings.General.CrimeLogFormat, player.username, victim ? victim.username: "No victim", crime.crimeName), player.username, 
-                embeds: Core.Instance.Settings.General.CrimeUseEmbed? EmbedCrafter.CreateAllEmbeds(Core.Instance.Settings.General.CrimeEmbed,player) :null);
+            var crime = player.manager.GetCrime(crimeIndex);
+            Core.Instance.SendDefaultEvent(DefaultEvents.OnCrime, player.username, victim ? victim.username : "No victim", crime.crimeName);
         }
     }
 }
--- src/Webhooks/Events/OnStarted.cs
+++ src/Webhooks/Events/OnStarted.cs
@@ -8,6 +8,7 @@ namespace Webhooks.RegisteredEvents
         [Target(GameSourceEvent.ManagerStart, ExecutionMode.Event)]
         public void OnEvent(SvManager svManager)
         {
+            Core.Instance.SvManager = svManager;
             Core.Instance.JoinWebhook.Send(string.Format(Core.Instance.Settings.Server.ServerStartMessage),
                 embeds: Core.Instance.Settings.Server.ServerStartUseEmbed? Core.Instance.Settings.Server.ServerStartEmbed:null);
         }
